021daa4143f756604b86f214467f75c8531d0088
Perhenric Lönnberg <perhenric@gmail.com>
2018-08-09 21:12:02 +0200

    asdf

--- gitversion.sh
+++ gitversion.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 IFS=$'\n\t'
 
 function main() {
-	git diff HEAD^ HEAD swagger.yaml | grep version:
+	git diff HEAD^ HEAD swagger.yaml | grep version: | versionHunter
 }
 function versionHunter() {
 	echo "Hello"
