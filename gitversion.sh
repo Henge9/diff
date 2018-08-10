3518ef0fc5832ad511a796cb68b018265fa8e6ca
Perhenric Lönnberg <perhenric@gmail.com>
2018-08-10 14:59:23 +0200

    asdf

--- gitversion.sh
+++ gitversion.sh
@@ -3,11 +3,18 @@ set -euo pipefail
 IFS=$'\n\t'
 
 main() {
-	git diff HEAD^ HEAD swagger.yaml
+	git diff origin/develop -- HEAD swagger.yaml
 	if ! git diff --quiet HEAD^ HEAD swagger.yaml; then
 		echo "it has changed"
 	else
 		echo "exit 0"
+	fi
+
+	if ! git diff --quiet origin/"${BRANCH_NAME}" HEAD api/swagger/swagger.yaml; then
+		echo "swagger.yaml is changed building dart api."
+	else
+		echo "No changes in swagger.yaml, dart api will not be built."
+		exit 0
 	fi
 }
 main $@
