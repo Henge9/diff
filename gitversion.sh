54b386d3b372145942e09775ed9d8f5d2b917480
Perhenric Lönnberg <perhenric@gmail.com>
2018-08-10 13:05:15 +0200

    asdf

--- gitversion.sh
+++ gitversion.sh
@@ -6,6 +6,12 @@ main() {
 	git diff HEAD^ HEAD swagger.yaml
 	if ! git diff --quiet HEAD^ HEAD swagger.yaml; then
 		echo "it has changed"
+		git diff HEAD^ HEAD swagger.yaml | grep version:
+		if ! git diff --quiet HEAD^ HEAD swagger.yaml | grep version:; then
+			echo "version to"
+		else
+			echo "goto ..."
+		fi
 	else
 		echo "asdf"
 	fi
